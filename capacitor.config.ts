b5984c10c05c7c0b9ec846a11639fd506f5fedb0
Ryan Twohey <ryantwohey@Ryans-MBP-2.attlocal.net>
2023-06-01 14:54:29 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-react-tabs-starter',
+  webDir: 'build',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
