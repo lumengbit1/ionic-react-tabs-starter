b80660ec190fd389a28d39bced895e985946bcd2
Meng Lu <meng.lu@jetstar.com>
2023-06-05 14:50:22 +1000

    add css libs and Header component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,15 +1,30 @@
 import React from "react";
+import cx from "classnames";
 import { IonHeader, IonToolbar } from "@ionic/react";
 
-import * as styles from "./Header.module.css";
+import styles from "./header.module.scss";
 
 type HeaderProps = {
   children: React.ReactNode;
+  variant?: "default";
+  overlayed?: boolean;
 };
 
-const Header: React.FC<HeaderProps> = ({ children }) => (
-  <IonHeader>
-    <IonToolbar>{children}</IonToolbar>
+const Header: React.FC<HeaderProps> = ({
+  children,
+  variant = "default",
+  overlayed = true,
+}) => (
+  <IonHeader className={cx(styles.header, { [styles[variant]]: variant })}>
+    <IonToolbar
+      className={cx(styles.toolbar, {
+        [styles[variant]]: variant,
+        [styles.overlayed]: overlayed,
+        [styles.rounded]: variant === "default",
+      })}
+    >
+      {children}
+    </IonToolbar>
   </IonHeader>
 );
 
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,16 +1,25 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import React from "react";
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import Header from "../components/header/Header";
+import "./Tab1.css";
 
 const Tab1: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
-      </IonHeader>
+      <Header>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Tab 1</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+      </Header>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
